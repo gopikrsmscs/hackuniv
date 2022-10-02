ee478aa385f98ff1aae204f16476abb55d278e83
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-10-02 06:08:01 -0500

    Add cross origin

--- src/main/java/com/hackuniv/management/controller/CourseController.java
+++ src/main/java/com/hackuniv/management/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.hackuniv.management.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,7 @@ public class CourseController {
 		courseService.Create(courses);
 	}
 
+	@CrossOrigin
 	@PostMapping("register")
 	public void registerCourse(@RequestBody(required = true) UserCourse userCourse) throws Exception {
 		courseService.Create(userCourse);
